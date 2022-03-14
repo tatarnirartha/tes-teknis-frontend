3bb48d37f8fe7635870fb0a64e14c97f7aa1c341
tatarnirartha <tatarnirartha@gmail.com>
2022-03-14 12:34:21 +0800

    initial-commit

---
+++ resources/js/app.js
@@ -0,0 +1,18 @@
+require('./bootstrap');
+
+import Vue from "vue";
+import VueRouter from "vue-router";
+import router from "./router/index";
+import store from "./store/index";
+import App from"./App.vue";
+
+Vue.config.productionTip = false
+
+Vue.use(VueRouter);
+
+const app = new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App }
+})
